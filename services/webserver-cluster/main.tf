8a0faa0c8dfce312c0a3d1000c5a6326242d4949
Peter Altosaar <peter.alrtosaar@gmail.com>
2021-03-21 16:57:21 -0400

    Remove backend configuration from module

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-2"
-
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "us-east-2"
 }
