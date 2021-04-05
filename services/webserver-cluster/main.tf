d991716c01af0c46fab707403063def81ad6afed
Peter Altosaar <peter.alrtosaar@gmail.com>
2021-04-05 15:59:32 -0400

    Use for_each to create dynamic tags on autoscaling group

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -64,6 +64,16 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-${var.cluster_name}"
     propagate_at_launch = true
   }
+
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_security_group" "instance" {
